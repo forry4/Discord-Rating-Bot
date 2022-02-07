0c0b2742f8407d28966309a39963a5668b682977
Forrest McElroy <forry4@gmail.com>
2022-02-07 16:08:08 -0500

    More comments

--- SAP_Bot.py
+++ SAP_Bot.py
@@ -128,13 +128,12 @@ def getPlayers():
 
     #sort and display the dictionary
     players = {k: v for k, v in sorted(players.items(), key=lambda item: item[1][0], reverse=True)}
+    #add index to sorted dictionary
     i=1
     for player in players:
         players.get(player)[4] = i
         players[player] = players.get(player)
         i += 1
-        
-    print(players)
 
     return players
 
@@ -155,16 +154,23 @@ async def on_ready():
 @bot.event
 async def on_raw_reaction_add(payload):
     message_id = payload.message_id
+    #check users are reacting to certain message
     if message_id == 938584658105466921:
+        #get the id of the server
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g : g.id == guild_id, bot.guilds)
+        #see if users reacted with certain emote
         if payload.emoji.name == 'monkey':
+            #set the role to be added as 'Ranked'
             role = discord.utils.get(guild.roles, name='Ranked')
         else:
             role = discord.utils.get(guild.roles, name=payload.emoji.name)
+        #if we got a role from the previous step, add it
         if role is not None:
+            #get the id of the member that reacted
             member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
             if member is not None:
+                #add the role to the member
                 await member.add_roles(role)
                 print((f'{role} added to {member}'))
             else:
@@ -177,16 +183,23 @@ async def on_raw_reaction_add(payload):
 @bot.event
 async def on_raw_reaction_remove(payload):
     message_id = payload.message_id
+    #check users are reacting to certain message
     if message_id == 938584658105466921:
+        #get the id of the server
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g : g.id == guild_id, bot.guilds)
+        #see if users reacted with certain emote
         if payload.emoji.name == 'monkey':
+            #set the role to be added as 'Ranked'
             role = discord.utils.get(guild.roles, name='Ranked')
         else:
             role = discord.utils.get(guild.roles, name=payload.emoji.name)
+        #if we got a role from the previous step, add it
         if role is not None:
+            #get the id of the member that reacted
             member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
             if member is not None:
+                #remove the role from the member
                 await member.remove_roles(role)
                 print((f'{role} removed from {member}'))
             else:
@@ -198,14 +211,11 @@ async def on_raw_reaction_remove(payload):
 #print all members in the server
 @bot.command()
 async def members(ctx):
+    #check to see if I am giving the command
     if ctx.author.id == 292116181312339969:
         members = {}
+        #create dictionary of member ids and their discord name (Donutseeds#7704 for example)
         for member in ctx.guild.members:
-            if member.nick == 'pendis':
-                print(member)
-                print(member.nick)
-                print(member.name)
-                print(member.id)
             members[member.id] = str(member)
         print(members)
     return members
@@ -213,6 +223,7 @@ async def members(ctx):
 #return dictionary of member ids and member names
 def getMembers(ctx):
     members={}
+    #create dictionary of member ids and their discord display name (Donutseeds for example)
     for member in ctx.guild.members:
         members[member.id] = member.display_name
     return members
@@ -232,12 +243,16 @@ async def submit(ctx, *message):
             #convert tuple to list
             message = list(message)
             print(message)
+            #get the dictionary of member ids : display names
             members = getMembers(ctx)
             #open csv file to append new data
-            with open('ranking.csv', 'a', newline='') as f_object:
-                writer_object = writer(f_object)
+            with open('ranking.csv', 'a', newline='') as ranking:
+                writer_object = writer(ranking)
+                #keep track of players in the submitted lobby
                 currentPlayers={}
+                #loop through each player in the lobby
                 for i in range(0,len(message),2):
+                    #get the discord id from the submitted ping
                     username = f'{message[i][3:-1]}#'
                     print(f'username: {username}')
                     line = []
@@ -245,15 +260,16 @@ async def submit(ctx, *message):
                     line.append(username)
                     line.append(gameID)
                     line.append(message[i+1])
-                    #keep track of players in the lobby
+                    #add the current player's rating (prior to this submission) to the dictionary
                     try:
                         currentPlayers[username] = [players.get(username)[0],0]
+                    #if this is their first game, give them a base rating (1200)
                     except:
                         currentPlayers[username] = [3.6666+25-2*8.3333,0]
                     print(line)
                     #append line to csv file
                     writer_object.writerow(line)
-                f_object.close()
+                ranking.close()
             #confirmation message
             await ctx.channel.send(f'Thank you {author} for submitting gameID {gameID}!')
         except Exception as e:
@@ -264,9 +280,11 @@ async def submit(ctx, *message):
         #update player rankings
         players = getPlayers()
         for player in currentPlayers:
+            #set the current players new rating after the lobby
             currentPlayers.get(player)[1] = players.get(player)[0]
             currentPlayers[player] = currentPlayers.get(player)
             print(currentPlayers[player])
+        #sort the list of current players by their new rating
         currentPlayers = {k: v for k, v in sorted(currentPlayers.items(), key=lambda item: item[1][1], reverse=True)} 
         print(f'currentPlayers: {currentPlayers}')
         playerRank = (f'```\n#  Player              Rating\n')
@@ -275,15 +293,20 @@ async def submit(ctx, *message):
         for player in currentPlayers:
             print(f'player: {player}')
             print(players.get(str(player)))
-            #return the matching rank and elo
+            #return the matching rank
             playerRank += (f'{players.get(player)[4]}   ')
+            #remove spaces from string the longer thier rank index is
             for j in range(len(str(players.get(player)[4]))):
                 playerRank = playerRank[:-1]
+            #add username to the string
             playerRank += members.get(int(player[:-1]))
+            #add spaces for proper allignment
             for k in range(20 - len(members.get(int(player[:-1])))):
                 playerRank += ' '
+            #get the new rating and calculate the change
             rating = 100*currentPlayers.get(player)[1]
             change = rating - 100*currentPlayers.get(player)[0]
+            #add change to the string
             playerRank += (f'{int(rating)}(')
             if change > 0:
                 playerRank += '+'
@@ -299,6 +322,7 @@ async def submit(ctx, *message):
                     #get role to add/remove
                     role1 = discord.utils.get(ctx.author.guild.roles, name = "High Elo Gamer")
                     role2 = discord.utils.get(ctx.author.guild.roles, name = "Mid Elo Gamer")
+                    #add high elo role if above 30, add mid elo role if above 27.5
                     if players.get(player)[0]<30:
                         await member.remove_roles(role1)
                         if players.get(player)[0]<27.5:
@@ -311,6 +335,7 @@ async def submit(ctx, *message):
                     break
     return
 
+#command to replace certain cells in the excel spreadsheet; change the function as necessary
 @bot.command()
 async def replaceAll(ctx):
     if ctx.author.id == 292116181312339969:
@@ -320,12 +345,6 @@ async def replaceAll(ctx):
             text = ''.join([i for i in text]) 
             # search and replace the contents
             for member in ctx.guild.members:
-                # if member.nick is not None:
-                #     print(member.nick)
-                # if member.nick is not None and member.nick.strip() in text:
-                #     text = text.replace(member.nick, str(member))
-                # elif member.name.strip() in text:
-                #     text = text.replace(member.name, str(member))
                 if str(member).strip() in text:
                     text = text.replace((str(member)), f"{member.id}#")
             # write to output file
@@ -334,6 +353,7 @@ async def replaceAll(ctx):
             x.close()
     return
 
+#replace an occurence in the spreadsheet with different text
 @bot.command()
 async def replace(ctx, nameOld, nameNew):
     if ctx.author.id == 292116181312339969:
@@ -366,21 +386,22 @@ async def deleteGame(ctx, gameID):
 #check what rank a specified user is
 @bot.command()
 async def search(ctx, message):
-    #get name of specified user
+    #get list of members
     members = getMembers(ctx)
     print(f'len message: {len(message[3:-1])}')
+    #for some reason when certain members use the search command, their message differs in length
     if len(message[3:-1]) == 18:
         username = f'{message[3:-1]}#'
         print(f'18: {username}')
     else:
         username = f'{message[2:-1]}#'
         print(f'not 18: {username}')
-    print(f'username: {username}')
-    print(f'member: {members.get(int(username[:-1]))}')
+    #update list of players
     players = getPlayers()
     i=1
     #check to see if any player in the system matches the user's name
     for player in players:
+        #check if searched player is in the system
         if player == username:
             #return the matching rank and elo
             playerRank = (f'```\n#  Player              Rating\n{i}   ')
@@ -403,12 +424,14 @@ async def searchstats(ctx, message):
     #get name of specified user
     members = getMembers(ctx)
     print(f'len message: {len(message[3:-1])}')
+    #for some reason when certain members use the search command, their message differs in length
     if len(message[3:-1]) == 18:
         username = f'{message[3:-1]}#'
         print(f'18: {username}')
     else:
         username = f'{message[2:-1]}#'
         print(f'not 18: {username}')
+    #update list of players
     players = getPlayers()
     i=1
     #check to see if any player in the system matches the user's name
@@ -433,8 +456,10 @@ async def searchstats(ctx, message):
 #check top 10 players on the leaderboard
 @bot.command()
 async def leaderboard(ctx):
+    #check if there is no data in the spreadsheet
     if getGameID() == 0:
         await ctx.channel.send('No data in the leaderboard')
+    #get list of players and members
     players = getPlayers()
     members = getMembers(ctx)
     message = '```\n#  Player          Rating\n'
@@ -457,8 +482,10 @@ async def leaderboard(ctx):
 #check top 10 players on the leaderboard and give extended stats
 @bot.command()
 async def leaderboardstats(ctx):
+    #check if there is no data in the spreadsheet
     if getGameID() == 0:
         await ctx.channel.send('No data in the leaderboard')
+    #get list of players and members
     players = getPlayers()
     members = getMembers(ctx)
     message = '```\n#  Player              Rating  μ     σ    games\n'
